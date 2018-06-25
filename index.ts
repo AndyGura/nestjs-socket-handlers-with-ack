efbab1a6d981029422ff2d5d13a4bb8898ab9110
AndyGura <andy.gura.jd@gmail.com>
2018-06-26 01:23:25 +0300

    moved sources again

--- index.ts
+++ index.ts
@@ -1,1 +1,31 @@
-export * from './src/subscribe-message-with-ack.decorator';
+import { SubscribeMessage } from '@nestjs/websockets';
+import { SocketRequestModel } from './src/socket-request.model';
+
+export const SubscribeMessageWithAck: <T = string>(message: T) => MethodDecorator = (message) => {
+    const defaultDecorator: MethodDecorator = SubscribeMessage(message);
+    return (target, key, descriptor) => {
+        const result: any = defaultDecorator(target, key, descriptor);
+        const func: Function = result.value;
+        result.value = function (socket, payload) {
+            const req = new SocketRequestModel(payload);
+            func.bind(this)(socket, req.data)
+                .then(
+                    (msg) => {
+                        req.callback({
+                            success: true,
+                            msg: msg
+                        });
+                    })
+                .catch((err) => {
+                        req.callback({
+                            success: false,
+                            msg: err.message
+                        });
+                    }
+                );
+        };
+        Reflect.defineMetadata("__isMessageMapping", true, descriptor.value);
+        Reflect.defineMetadata("message", message, descriptor.value);
+        return result;
+    };
+};
